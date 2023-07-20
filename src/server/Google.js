727d85fdbbd64aabff75e78011d637789c860d24
dor933 <doratzabi1@gmail.com>
2023-07-20 16:19:59 +0300

    comazz

--- src/server/Google.js
+++ src/server/Google.js
@@ -2,42 +2,27 @@ const fs = require('fs');
 const { Storage } = require("@google-cloud/storage");
 require('dotenv').config();
 
-const base64key= process.env.NEWPRIVATEKEY;
+
+const bucketname = process.env.BUCKET_NAME;
+
+
+async function uploadmessage (myfile) {
+
+    const base64key= process.env.NEWPRIVATEKEY;
 const buff = Buffer.from(base64key, 'base64');
 const key = buff.toString('utf-8');
 
-let serviceAccount= {
-    type: process.env.TYPE,
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: key,
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    auth_uri: process.env.AUTH_URI ,
-    token_uri: process.env.TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url:process.env.CLIENT_X509_CERT_URL,
-    universe_domain: process.env.UNIVERSE_DOMAIN
+console.log("key", key);
 
-}
+
 
 
 const storage = new Storage( {
     projectId: process.env.PROJECT_ID,
-    credentials: serviceAccount
+    credentials: key
 }
 );
-const bucketname = process.env.BUCKET_NAME;
 
-  console.log("bucketname", bucketname);
-
-async function uploadmessage (myfile) {
-
-    console.log("myfile", myfile)
-
-
-
-    console.log("bucketname", bucketname);
 
 
     await storage.bucket(bucketname).upload(myfile, {
