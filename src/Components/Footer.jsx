969cf4654772d89e461ae23e0422f34871ccc560
Dor Ratzabi <dor.r@banxso.com>
2024-03-21 14:40:43 +0200

    comamit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,10 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { useState,useContext,useEffect } from 'react';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import Faceicon from '../assets/images/social_media/Facebook.png';
+import Instagram from '../assets/images/social_media/Instagram.png';
+import Twitter from '../assets/images/social_media/Twitter.png';
+import LinkedIn from '../assets/images/social_media/LinkedIn.png';
 
 
 
@@ -17,14 +18,155 @@ const Footer = () => {
             display: 'inline-flex',
             flexDirection: 'column',
             alignItems:'flex-start',
-            height:"20%",
+            height:"40%",
             paddingTop:"106.667px",
             paddingBottom:"106.667px",
             paddingLeft:"85.333px",
             paddingRight:"85.333px",
 
         }}>
-sadsa
+
+            <Grid container item xs={12} style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent:'space-between'
+            }}>
+
+                <Grid item xs={2} style={{
+                    display:'flex',
+                    flexDirection:'column',
+                    justifyContent:'flex-start',
+                    alignItems:'flex-start',
+                }}>
+                    test
+                </Grid>
+
+                <Grid container item xs={6} style={{
+                    display:'flex',
+                    flexDirection:'column',
+                    justifyContent:'flex-start',
+                    alignItems:'flex-start',
+                    flexDirection:'row',
+                }}>
+
+                    <Grid item xs={3} style={{
+                        display:'flex',
+                        flexDirection:'column',
+                        justifyContent:'flex-start',
+                        alignItems:'flex-end',
+                    }}>
+                        Home
+
+                        </Grid>
+
+                        <Grid item xs={3} style={{
+                        display:'flex',
+                        flexDirection:'column',
+                        justifyContent:'flex-start',
+                        alignItems:'flex-end',
+                    }}>
+                        Portfolio
+
+                        </Grid>
+
+                        <Grid item xs={3} style={{
+                        display:'flex',
+                        flexDirection:'column',
+                        justifyContent:'flex-start',
+                        alignItems:'flex-end',
+                    }}>
+                        About
+
+                        </Grid>
+
+                        <Grid item xs={3} style={{
+                        display:'flex',
+                        flexDirection:'column',
+                        justifyContent:'flex-start',
+                        alignItems:'flex-end',
+                    }}>
+                        Contact
+
+                        </Grid>
+                    
+                    
+                    </Grid>
+
+                    <Grid container item xs={4} style={{
+                    display:'flex',
+                    flexDirection:'row',
+                    justifyContent:'flex-end',
+                    alignItems:'flex-start',
+                }}>
+                    <Grid item xs={2} style={{
+                        display:'flex',
+                        flexDirection:'column',
+                        justifyContent:'flex-start',
+                        alignItems:'flex-end',
+                    }}>
+                        
+                        <img src={Faceicon}
+                        style={{
+                            width:"32px",
+                            height:"32px",
+
+                        }} />
+
+                        </Grid>
+                        <Grid item xs={2} style={{
+                        display:'flex',
+                        flexDirection:'column',
+                        justifyContent:'flex-start',
+                        alignItems:'flex-end',
+                    }}>
+                        
+                        <img src={Instagram}
+                        style={{
+                            width:"32px",
+                            height:"32px",
+
+                        }} />
+
+                        </Grid>
+                        <Grid item xs={2} style={{
+                        display:'flex',
+                        flexDirection:'column',
+                        justifyContent:'flex-start',
+                        alignItems:'flex-end',
+                    }}>
+                        
+                        <img src={Twitter}
+                        style={{
+                            width:"32px",
+                            height:"32px",
+
+                        }} />
+
+                        </Grid>
+                        <Grid item xs={2} style={{
+                        display:'flex',
+                        flexDirection:'column',
+                        justifyContent:'flex-start',
+                        alignItems:'flex-end',
+                    }}>
+                        
+                        <img src={LinkedIn}
+                        style={{
+                            width:"32px",
+                            height:"32px",
+
+                        }} />
+
+                        </Grid>
+
+
+
+                    </Grid>
+                
+
+                </Grid>
+
             </Grid>
 
     );
